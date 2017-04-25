363f9edef1f33545afbfef5bf55e925bd0fe3275
Alassane Diop <adiop005@emi.u-bordeaux1.fr>
2017-04-25 17:21:33 +0200

    .

--- programmation_parallele/PROJET-VIE/fichiers/src/compute.c
+++ programmation_parallele/PROJET-VIE/fichiers/src/compute.c
@@ -47,97 +47,193 @@ unsigned opencl_used [] = {
 ///////////////////////////// Version séquentielle simple
 
 
+/* unsigned compute_v0 (unsigned nb_iter) */
+/* {  */
+/* 	int	ti = 0; */
+/* 	int count=0; */
+/*     for (int i = 1; i < DIM-1; i++) */
+/*     { */
+/*     	for (int j = 1; j < DIM-1; j++) */
+/* 		{ */
+/* 	 */
+/* 		 */
+/* 			//cas ou la cellule est vivante */
+/* 			if (cur_img(i,j)) */
+/* 			{ */
+/* 				for (int a=i-1;a<=i+1;a++) */
+/* 				{ */
+/* 					for(int b=j-1;b<=j+1;b++) if ((i!=a || j!=b) && (cur_img(a,b)==0xFFFF00FF)) count++; */
+/* 				} */
+/* 				#<{(| printf("%d) a. vivante:  pixel(%d,%d) voisins count :%d \n",ti,i,j,count); |)}># */
+/* 				//si elle n a pas 2 ou 3 voisins la cellule meurt */
+/* 				if (count == 2 || count == 3) next_img(i,j)=0xFFFF00FF; */
+/* 				else next_img(i,j)=0x0; */
+/* 			} */
+/* 			 */
+/* 	 */
+/* 			else */
+/* 			{ */
+/* 				//cas ou la cellule est morte */
+/* 				for (int a=i-1;a<=i+1;a++) */
+/* 				{ */
+/* 					for(int b=j-1;b<=j+1;b++) */
+/* 					{ */
+/* 						if ((i!=a && j!=b) && (cur_img(a,b)!=0x0)) count++; */
+/* 					} */
+/* 				} */
+/* 				#<{(| printf("%d) b. morte : pixel(%d,%d) voisins count :%d \n",ti,i,j,count); |)}># */
+/* 				//si elle n a pas 3 voisins la cellule reste morte */
+/* 				if (count ==3) next_img(i,j)=0xFFFF00FF; */
+/* 				else next_img(i,j)=cur_img(i,j); */
+/* 			} */
+/* 		 */
+/* 		count=0; */
+/* 		} */
+/* 	} */
+/*      swap_images (); */
+/*       */
+/*       */
+/*    */
+/*   // retourne le nombre d'étapes nécessaires à la */
+/*   // stabilisation du calcul ou bien 0 si le calcul n'est pas */
+/*   // stabilisé au bout des nb_iter itérations */
+/*   return 0; */
+/* } */
+
+
+
 unsigned compute_v0 (unsigned nb_iter)
 {
- for (unsigned it = 1; it <= nb_iter; it ++) {
-    for (int i = 0; i < DIM; i++)
-      for (int j = 0; j < DIM; j++) {
-//	next_img (i, j) = cur_img (j, i);
+	int count=0;
 
-	      if ((i>0 && i<DIM-2) && (j>0 && j<DIM-2)) { //test pour ne pas prendre les bords du tableau
+    for (int i = 1; i < DIM-1; i++)
+    {
+    	for (int j = 1; j < DIM-1; j++)
+		{
+			for (int a=i-1;a<=i+1;a++)
+				for(int b=j-1;b<=j+1;b++) 
+					if ( i != a || j != b ) 
+						if(cur_img(a,b)!=0x0) 
+							count++;
+			
+			if (cur_img(i,j) != 0x0) { //cas ou la cellule est vivante
+				
+				//si elle n a pas 2 ou 3 voisins la cellule meurt
+				if (count == 2 || count == 3)
+					next_img(i,j)=cur_img(i,j);
+				else
+					next_img(i,j)=0x0;
 
-		      // rendre une cellule morte vivante
-		      if ((cur_img(i, j) & 0xFF) == 0) {
-				      printf("cellule morte\n");
-		      }
+			} else { //cas ou la cellule est morte
 
-		      //rendre une cellule vivante morte
-		      else/* (cur_img(i, j) == 0xFFFF00FF)*/ {	
-				      printf("cellule vivante \n");
-		      }
-	      }
-      }
-
-    
-    swap_images ();
-  }
-
-
-  for (unsigned it = 1; it <= nb_iter; it ++) {
-    for (int i = 0; i < DIM; i++)
-      for (int j = 0; j < DIM; j++) {
-//	next_img (i, j) = cur_img (j, i);
-
-	      if ((i>0 && i<DIM-2) && (j>0 && j<DIM-2)) { //test pour ne pas prendre les bords du tableau
-
-		      // rendre une cellule morte vivante
-		      if ((cur_img(i, j) & 0xFF) == 0) {
-			      int control_sum = 0;
-			      control_sum += cur_img(i-1, j-1) + cur_img(i-1, j) + cur_img(i-1, j+1) +
-			      		     cur_img(i, j-1) + cur_img(i, j+1) +
-			      		     cur_img(i+1, j-1) + cur_img(i+1, j) + cur_img(i+1, j+1) ;
-
-			      if (control_sum == 3) { // la cellule devient VIVANTE
-				      cur_img(i, j) = 0xFFFF00FF|0xFF;
-				      printf("cellule morte à rendre vivante\n");
-			      }
-		      }
-
-		      //rendre une cellule vivante morte
-		      else /*(cur_img(i, j) == 0xFFFF00FF)*/ {	
-			      int control_sum = 0;
-			      control_sum += cur_img(i-1, j-1) + cur_img(i-1, j) + cur_img(i-1, j+1) +
-			      		     cur_img(i+1, j-1) + cur_img(i+1, j) + cur_img(i+1, j+1) +
-			      		     cur_img(i, j-1) + cur_img(i, j+1) ;
-
-			      if (control_sum != 2 && control_sum != 3) { // la cellule devient MORTE
-				      cur_img(i, j) = 0;
-				      printf("cellule vivante à rendre morte \n");
-			      }
+				//si elle n a pas 3 voisins la cellule reste morte
+				if (count ==3)
+					next_img(i,j)=0xFFFF00FF;
+				else
+					next_img(i,j)=cur_img(i,j);
+			}
+		
+			count=0;
+		}
+	}
+     swap_images ();
 
 
 
-		      }
-	      }
-      }
 
-    
-    swap_images ();
-  }
 
-  for (unsigned it = 1; it <= nb_iter; it ++) {
-    for (int i = 0; i < DIM; i++)
-      for (int j = 0; j < DIM; j++) {
-//	next_img (i, j) = cur_img (j, i);
 
-	      if ((i>0 && i<DIM-2) && (j>0 && j<DIM-2)) { //test pour ne pas prendre les bords du tableau
+/*  for (unsigned it = 1; it <= nb_iter; it ++) { */
+/*     for (int i = 0; i < DIM; i++) */
+/*       for (int j = 0; j < DIM; j++) { */
+/* //	next_img (i, j) = cur_img (j, i); */
+/*  */
+/* 	      if ((i>0 && i<DIM-2) && (j>0 && j<DIM-2)) { //test pour ne pas prendre les bords du tableau */
+/*  */
+/* 		      // rendre une cellule morte vivante */
+/* 		      if ((cur_img(i, j) & 0xFF) == 0) { */
+/* 				      printf("cellule morte\n"); */
+/* 		      } */
+/*  */
+/* 		      //rendre une cellule vivante morte */
+/* 		      else#<{(| (cur_img(i, j) == 0xFFFF00FF)|)}># { */
+/* 				      printf("cellule vivante \n"); */
+/* 		      } */
+/* 	      } */
+/*       } */
+/*  */
+/*  */
+/*     swap_images (); */
+/*   } */
 
-		      // rendre une cellule morte vivante
-		      if ((cur_img(i, j) & 0xFF) == 0) {
-				      printf("cellule morte\n");
-		      }
 
-		      //rendre une cellule vivante morte
-//		      if (cur_img(i, j) == 0xFFFF00FF) {	
-		      else {
-				      printf("cellule vivante \n");
-		      }
-	      }
-      }
-
-    
-    swap_images ();
-  }
+/*   for (unsigned it = 1; it <= nb_iter; it ++) { */
+/*     for (int i = 0; i < DIM; i++) */
+/*       for (int j = 0; j < DIM; j++) { */
+/* //	next_img (i, j) = cur_img (j, i); */
+/*  */
+/* 	      if ((i>0 && i<DIM-2) && (j>0 && j<DIM-2)) { //test pour ne pas prendre les bords du tableau */
+/*  */
+/* 		      // rendre une cellule morte vivante */
+/* 		      if ((cur_img(i, j) & 0xFF) == 0) { */
+/* 			      int control_sum = 0; */
+/* 			      control_sum += cur_img(i-1, j-1) + cur_img(i-1, j) + cur_img(i-1, j+1) + */
+/* 			      		     cur_img(i, j-1) + cur_img(i, j+1) + */
+/* 			      		     cur_img(i+1, j-1) + cur_img(i+1, j) + cur_img(i+1, j+1) ; */
+/*  */
+/* 			      if (control_sum == 3) { // la cellule devient VIVANTE */
+/* 					  cur_img(i, j) = 0xFFFF00FF|0xFF; */
+/* 					  printf("cellule morte à rendre vivante\n"); */
+/* 					  next_img (i, j) = cur_img (j, i); */
+/* 			      } */
+/* 		      } */
+/*  */
+/* 		      //rendre une cellule vivante morte */
+/* 		      else #<{(|(cur_img(i, j) == 0xFFFF00FF)|)}># {	 */
+/* 			      int control_sum = 0; */
+/* 			      control_sum += cur_img(i-1, j-1) + cur_img(i-1, j) + cur_img(i-1, j+1) + */
+/* 			      		     cur_img(i+1, j-1) + cur_img(i+1, j) + cur_img(i+1, j+1) + */
+/* 			      		     cur_img(i, j-1) + cur_img(i, j+1) ; */
+/*  */
+/* 			      if (control_sum != 2 && control_sum != 3) { // la cellule devient MORTE */
+/* 				      cur_img(i, j) = 0; */
+/* 				      printf("cellule vivante à rendre morte \n"); */
+/* 					  next_img (i, j) = cur_img (j, i); */
+/* 			      } */
+/*  */
+/*  */
+/*  */
+/* 		      } */
+/* 	      } */
+/*       } */
+/*  */
+/*  */
+/*     swap_images (); */
+/*   } */
+/*  */
+/*   for (unsigned it = 1; it <= nb_iter; it ++) { */
+/*     for (int i = 0; i < DIM; i++) */
+/*       for (int j = 0; j < DIM; j++) { */
+/* //	next_img (i, j) = cur_img (j, i); */
+/*  */
+/* 	      if ((i>0 && i<DIM-2) && (j>0 && j<DIM-2)) { //test pour ne pas prendre les bords du tableau */
+/*  */
+/* 		      // rendre une cellule morte vivante */
+/* 		      if ((cur_img(i, j) & 0xFF) == 0) { */
+/* 				      printf("cellule morte\n"); */
+/* 		      } */
+/*  */
+/* 		      //rendre une cellule vivante morte */
+/* //		      if (cur_img(i, j) == 0xFFFF00FF) {	 */
+/* 		      else { */
+/* 				      printf("cellule vivante \n"); */
+/* 		      } */
+/* 	      } */
+/*       } */
+/*  */
+/*  */
+/*     swap_images (); */
+/*   } */
 
   // retourne le nombre d'étapes nécessaires à la
   // stabilisation du calcul ou bien 0 si le calcul n'est pas
@@ -150,13 +246,55 @@ unsigned compute_v0 (unsigned nb_iter)
 
 void first_touch_v1 ()
 {
-  int i,j ;
+  int i,j;
 
 #pragma omp parallel for
   for(i=0; i<DIM ; i++) {
     for(j=0; j < DIM ; j += 512)
       next_img (i, j) = cur_img (i, j) = 0 ;
   }
+
+
+/* 	int count=0; */
+/*  */
+/* #pragma omp parallel for */
+/* 	for (int i = 1; i < DIM-1; i++) */
+/*     { */
+/*     	for (int j = 1; j < DIM-1; j+=512) */
+/* 		{ */
+/* 			for (int a=i-1;a<=i+1;a++) */
+/* 				for(int b=j-1;b<=j+1;b++)  */
+/* 					if ( i != a || j != b )  */
+/* 						if(cur_img(a,b)!=0x0) */
+/* #<{(| #pragma omp critical |)}># */
+/* 							count++; */
+/* 			 */
+/* 			if (cur_img(i,j) != 0x0) { //cas ou la cellule est vivante */
+/* 				 */
+/* 				//si elle n a pas 2 ou 3 voisins la cellule meurt */
+/* 				if (count == 2 || count == 3) */
+/* #<{(| #pragma omp critical |)}># */
+/* 					next_img(i,j)=cur_img(i,j); */
+/* 				else */
+/* #pragma omp critical */
+/* 					next_img(i,j)=0x0; */
+/*  */
+/* 			} else { //cas ou la cellule est morte */
+/*  */
+/* 				//si elle n a pas 3 voisins la cellule reste morte */
+/* 				if (count ==3) */
+/* #pragma omp critical */
+/* 					next_img(i,j)=0xFFFF00FF; */
+/* 				else */
+/* #pragma omp critical */
+/* 					next_img(i,j)=cur_img(i,j); */
+/* 			} */
+/* 		 */
+/* #pragma omp critical */
+/* 			count=0; */
+/* 		} */
+/* 	} */
+/*      swap_images (); */
 }
 
 // Renvoie le nombre d'itérations effectuées avant stabilisation, ou 0
